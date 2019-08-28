d38f4199154c889bf9d3092e046a4ea5533013b4
cjanarthanangit <janaece_12l@rocketmail.com>
2019-08-28 15:16:48 +0530

    Adding DIVs and Contents... With Flex

--- 6/index.php
+++ 6/index.php
@@ -20,7 +20,24 @@
 			<label for="favColor">Favorite Color:</label>
 			<input type="text" id="favColor">
 		</div>
-	</form>
+	</form>		
+</div>
+<div class="column-layout">
+	<div class="main-column">
+		<h2>Main Column</h2>
+		<p>You can use the mouse to resize the outer frame and to move and resize data ... In addition, you can use the Chart Size tab on the Properties window to enter exact ... When a four-headed arrow appears, click and drag the element to a new location</p>
+		<p>Drag a window title bar to the left or right edge of the screen, and it ... to one edge of the display, Windows shows other open windows in ... Here are a few ways you can snap windows in Windows 10 by using a mouse or by dragging ... the edge, the window snaps to full height without changing its width.</p>
+		<p>Height/Width. Enter the height and width for the chart (outer frame). The measurement unit comes from the setting on the General tab of the Options dialog box (Edit menu in the Viewer). Experiment to get the size that works best for your chart. If you make the chart too small, elements can overlap. Also note that resizing the outer frame resizes other chart elements such as the data frame and legend frame.</p>
+	</div>
+	<div class="sidebar-one">
+		<h2>Side Column1</h2>
+		<p>You can use the mouse to resize the outer frame and to move and resize data ... In addition, you can use the Chart Size tab on the Properties window to enter exact ... When a four-headed arrow appears, click and drag the element to a new location</p>
+	</div>
+	<div class="sidebar-two">
+		<h2>Side Column2</h2>
+		<p>You can use the mouse to resize the outer frame and to move and resize data ... In addition, you can use the Chart Size tab on the Properties window to enter exact ... When a four-headed arrow appears, click and drag the element to a new location</p>
+	</div>
+</div>	
 </div>
 </body>
 </html>
