5bbeb9d598b99d7ccd2078abe3cdad9a1c737529
cjanarthanangit <janaece_12@rocketmail.com>
2019-08-28 12:41:56 +0530

    Before Applying Flex at 6.

---
+++ 6/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="js/jquery-3.4.1.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	
+	
+</head>
+<body>
+<div class="container">
+	<form>
+		<div class="form-row">
+			<label for="name">Name:</label>
+			<input type="text" id="name">
+		</div>
+		<div class="form-row">
+			<label for="favColor">Favorite Color:</label>
+			<input type="text" id="favColor">
+		</div>
+	</form>
+</div>
+</body>
+</html>
