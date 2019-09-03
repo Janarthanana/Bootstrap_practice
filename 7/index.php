8d6c10daf1bbbdc0c2ee10b0c5cc2ff95c94acbb
cjanarthanangit <janaece_12l@rocketmail.com>
2019-09-03 11:58:37 +0530

    Blinking menu 8 and propeller at 7.

---
+++ 7/index.php
@@ -0,0 +1,35 @@
+<?php
+$ih = "http://etekchnoservices.com/"; 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Support</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	
+</head>
+<body>
+ 	<!-- <object style="height: 1500px;" data="https://skyviu.on.spiceworks.com/portal/tickets" width="100%"> <embed src="https://skyviu.on.spiceworks.com/portal/tickets" width="100%" height="100%"> </embed></object>
+ -->
+
+ <div class="menu pmd-floating-action"  role="navigation"> 
+	<a href="javascript:void(0);" class="pmd-floating-action-btn btn btn-sm pmd-btn-fab pmd-btn-raised pmd-ripple-effect btn-info" data-title="Support"> 
+		<span class="pmd-floating-hidden">Support</span>
+		<i class="material-icons">supervisor_account</i>
+	</a> 
+	<a href="javascript:void(0);" class="pmd-floating-action-btn btn btn-sm pmd-btn-fab pmd-btn-raised pmd-ripple-effect btn-info" data-title="Home"> 
+		<span class="pmd-floating-hidden">Cloud Queue</span> 
+		<i class="material-icons">home</i> 
+	</a> 
+	<a class="pmd-floating-action-btn btn pmd-btn-fab pmd-btn-raised pmd-ripple-effect btn-primary" data-title="Add" href="javascript:void(0);"> 
+		<span class="pmd-floating-hidden">Primary</span>
+		<i class="material-icons pmd-sm">add</i> 
+	</a> 
+</div>
+
+ 	<object style="height: 900px;" data="<?php echo $ih; ?>" width="100%"> <embed src="<?php echo $ih; ?>" width="100%" height="100%"> </embed></object>
+	<script src="js/jquery-3.4.1.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+</body>
+</html>
